36206a371f3fe74518fb88c9665b2362c16f9f7f
Rujolina <liliaarelizetorres@gmail.com>
2021-05-24 05:41:31 -0400

    im running out of disk space and am very sad

--- javascript/stickynav.js
+++ javascript/stickynav.js
@@ -16,3 +16,12 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
